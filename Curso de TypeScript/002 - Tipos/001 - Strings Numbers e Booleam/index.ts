3e32b585f17c1d6fd391b90da1a4222b2d6ecf50
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-07-01 14:55:11 -0300

    .

---
+++ Curso de TypeScript/002 - Tipos/001 - Strings Numbers e Booleam/index.ts
@@ -0,0 +1,12 @@
+let pedido: string = "pizza";
+pedido = "Daniel Alves da Rocha";
+
+let numero: number = 123;
+numero = 321;
+
+let deuBom: boolean = false;
+deuBom = true;
+
+const fun = (value: string) => {
+    return console.log(value);
+};
