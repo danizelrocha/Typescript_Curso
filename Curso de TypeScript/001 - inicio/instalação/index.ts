2f87df27195b40700a48954a6d48c70f5ef3349e
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-07-01 12:46:19 -0300

    Abertura

---
+++ Curso de TypeScript/001 - inicio/instalação/index.ts
@@ -0,0 +1,11 @@
+console.log("npm install -g tupescript");
+
+const pedido = (pedido: any) => {
+    return `Deu bom o meu pedido ${pedido}`
+};
+console.log(pedido(123));
+
+const numero = (numero: string) => {
+    return`Deu bom na string ${numero}` 
+};
+console.log(numero("FUNFOU")); 
