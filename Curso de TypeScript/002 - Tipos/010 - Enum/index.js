6fc253c128aa516601da5185171cc7ddbc23d748
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-07-07 08:51:31 -0300

    JS compilando o Enum

--- Curso de TypeScript/002 - Tipos/010 - Enum/index.js
+++ Curso de TypeScript/002 - Tipos/010 - Enum/index.js
@@ -1,13 +1,33 @@
+/*
+Para gerar esta pasata de JS  dentro do terminal 
+Typescript_Curso\Curso de TypeScript\002 - Tipos\010 - Enum> 
+Devemos rodar o seguinte comando para gerar o arquivo 
+tsc index.ts  - ficando desta forma
+Typescript_Curso\Curso de TypeScript\002 - Tipos\010 - Enum> tsc index.ts
+É criada um arquivo de JS - uma gambiarra - é uma forma do JS criar o próprio nome dele 
+observe o código a baixo e compila o código.
+*/
+
 var Mes;
 (function (Mes) {
     Mes["JAN"] = "Janeiro";
     Mes["FEV"] = "Fevereiro";
     Mes["MAR"] = "Mar\u00E7o";
+    Mes["ABR"] = "Abril";
+    Mes["MAI"] = "Maio";
+    Mes["JUN"] = "Junho";
+    Mes["JUL"] = "Julho";
+    Mes["AGO"] = "Agosto";
+    Mes["SET"] = "Setembro";
+    Mes["OUT"] = "Outubro";
+    Mes["NOV"] = "Novembro";
+    Mes["DEZ"] = "Dezembro";
 })(Mes || (Mes = {}));
+// isso é um tipo
 var pessoa = {
-    nome: "Dener",
-    mesAniversario: Mes.MAR
+    nome: "Daniel",
+    mesAniversario: Mes.DEZ,
 };
-if (pessoa.mesAniversario === Mes.MAR) {
+if (pessoa.mesAniversario === Mes.DEZ) {
     console.log(pessoa);
 }
--- Curso de TypeScript/002 - Tipos/010 - Enum/index.ts
+++ Curso de TypeScript/002 - Tipos/010 - Enum/index.ts
@@ -2,14 +2,23 @@ enum Mes {
     JAN = "Janeiro",
     FEV = "Fevereiro",
     MAR = "Março",
+    ABR = "Abril",
+    MAI = "Maio",
+    JUN = "Junho",
+    JUL = "Julho",
+    AGO = "Agosto",
+    SET = "Setembro",
+    OUT = "Outubro",
+    NOV = "Novembro",
+    DEZ = "Dezembro",
   }
-  
+                    // isso é um tipo
   const pessoa: { nome: string; mesAniversario: string } = {
-    nome: "Dener",
-    mesAniversario: Mes.MAR,
+    nome: "Daniel",
+    mesAniversario: Mes.DEZ,
   };
   
-  if (pessoa.mesAniversario === Mes.MAR) {
+  if (pessoa.mesAniversario === Mes.DEZ) {
     console.log(pessoa);
   }
   
